c8c5ff30cf8ef71ebff354a38a22a9c23f98b523
Canul <canuladriana24@gmail.com>
2024-03-19 00:22:33 -0600

    Logica de la galeria

--- app/src/main/java/com/example/galeriaapp/MainActivity2.java
+++ app/src/main/java/com/example/galeriaapp/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.galeriaapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,43 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    private ImageView imageView;
+    private Button btnAnterior, btnSiguiente;
+    private int[] imageArray;
+    private int currentPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        imageView = findViewById(R.id.imageView);
+        btnAnterior = findViewById(R.id.anterior);
+        btnSiguiente = findViewById(R.id.siguiente);
+
+        imageArray = new int[] {R.drawable.alguien_esta_mintiendo, R.drawable.archivo81, R.drawable.athena};
+
+        imageView.setImageResource(imageArray[currentPosition]);
+
+        btnAnterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentPosition > 0) {
+                    currentPosition--;
+                    imageView.setImageResource(imageArray[currentPosition]);
+                }
+            }
+        });
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentPosition < imageArray.length - 1) {
+                    currentPosition++;
+                    imageView.setImageResource(imageArray[currentPosition]);
+                }
+            }
         });
     }
 }
